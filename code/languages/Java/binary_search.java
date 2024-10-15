4e9352b7b3f51f496f425cdd9ba4f04bd5af2546
levinkhho <125771265+levinkhho@users.noreply.github.com>
2024-10-15 13:11:11 -0400

    Update binary_search.java

--- code/languages/Java/binary_search.java
+++ code/languages/Java/binary_search.java
@@ -1,17 +1,9 @@
-/*
-Binary search is a method for finding the index of an element in an already-sorted array.
-It works by repeatedly halving the array, picking the half that contains our element based
-on whether the middle element is lesser or greater than it, until the element is found.
-It's one of the simplest examples of a "divide and conquer" algorithm, which is an algorithm
-that repeatedly reduces the problem into smaller sub-problems until a solution is found.
-*/
-
-class Binary_Search {
-    public static int binary_search_int(int[] arr, int x) {
+class BinarySearch {
+    public static int binarySearchInt(int[] arr, int x) {
         // Initialize the variables. start and end define our search space
-        int start, end, midpoint;
-        start = 0;
-        end = arr.length - 1;
+        int start = 0;
+        int end = arr.length - 1;
+        int midpoint;
         // The main loop. Each iteration we cut our search space in half
         while (start <= end) {
             // Check the midpoint of the search space
@@ -34,20 +26,22 @@ class Binary_Search {
     }
     
     public static void intTester() {
-        int i, x;
+        int i;
+        int x;
         // The sorted array we will search
-        int[] sorted_arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int[] sortedArr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
         // A list of numbers not in the array, to see if our function correctly returns -1
         int[] extras = {12, -2, 34};
-        for (i = 0; i < sorted_arr.length + extras.length; i += 1) {
-            if (i < sorted_arr.length) {
-                x = sorted_arr[i];
+        
+        for (i = 0; i < sortedArr.length + extras.length; i += 1) {
+            if (i < sortedArr.length) {
+                x = sortedArr[i];
+            } else {
+                x = extras[i - sortedArr.length];
             }
-            else {
-                x = extras[i - sorted_arr.length];
-            }
+            
             System.out.print("Searching for element: " + x + "; ");
-            int result = binary_search_int(sorted_arr, x);
+            int result = binarySearchInt(sortedArr, x);
             if (result != -1) {
                 System.out.println("Element found at index: " + result);
             } else {
